587e7f5271e55b88160358976d0b75f94624f171
Michael Hawkins <58923195+cookienocreams@users.noreply.github.com>
2024-05-19 09:26:14 -0500

    Delete target/debug directory

--- target/debug/deps/fcs_rs-7f442e63d55241fc.d
+++
@@ -1,8 +0,0 @@
-/home/cookienocreams/no_bs_fcs/target/debug/deps/libfcs_rs-7f442e63d55241fc.rmeta: src/lib.rs src/data.rs src/header.rs src/text.rs
-
-/home/cookienocreams/no_bs_fcs/target/debug/deps/fcs_rs-7f442e63d55241fc.d: src/lib.rs src/data.rs src/header.rs src/text.rs
-
-src/lib.rs:
-src/data.rs:
-src/header.rs:
-src/text.rs:
